1c2030f34b450ee2111ea2648c59b9120041c4ce
nick.chen <sakanasan20@gmail.com>
2022-07-08 15:57:59 +0800

    Initial commit

---
+++ src/main/java/tw/niq/app/SpringBootMicroGatewayApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/niq/app/SpringBootMicroGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
